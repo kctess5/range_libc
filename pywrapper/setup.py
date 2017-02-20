11d8d3bc76a594d764d9d27ed5aba9b1821db9d2
kctess5 <corey.walsh11@gmail.com>
2017-02-19 22:31:25 -0500

    mac fixes

--- pywrapper/setup.py
+++ pywrapper/setup.py
@@ -1,16 +1,23 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy, os, platform
+
+if platform.system().lower() == "darwin":
+	os.environ["MACOSX_DEPLOYMENT_TARGET"] = platform.mac_ver()[0]
+	os.environ["CC"] = "c++"
 
 setup(
 	ext_modules=[
 		Extension("range_lib", ["RangeLib.pyx","../vendor/lodepng/lodepng.cpp"], 
 			extra_compile_args = ["-w","-std=c++11", "-march=native", "-ffast-math", "-fno-math-errno", "-O3"],
+			extra_link_args = ["-std=c++11","-stdlib=libc++"],
 			include_dirs = [
 				"../",
 				"../vendor/distance_transform/",
 				"../vendor/distance_transform/include",
-				"../vendor/distance_transform/extern/DopeVector/include/"
+				"../vendor/distance_transform/extern/DopeVector/include/",
+				numpy.get_include()
 			],
 			depends=["../includes/*.h"],
 			language="c++",)],
